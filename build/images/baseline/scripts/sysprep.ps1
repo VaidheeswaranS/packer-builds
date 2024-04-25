0453a84b5ae7e797196199267a1886babe47e949
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2024-04-25 12:27:14 +0530

    changed the sleep times

--- build/images/baseline/scripts/sysprep.ps1
+++ build/images/baseline/scripts/sysprep.ps1
@@ -3,7 +3,8 @@ while ((Get-Service RdAgent).Status -ne 'Running') { Start-Sleep -s 5 }
 Write-Host '>>> Waiting for GA Service (WindowsAzureGuestAgent) to start ...'
 while ((Get-Service WindowsAzureGuestAgent).Status -ne 'Running') { Start-Sleep -s 5 }
 Write-Host '>>> Sysprepping VM ...'
-Start-Sleep -s 180
+# Start-Sleep -s 180
+Start-Sleep -s 60
 if(Test-Path $Env:SystemRoot\system32\Sysprep\unattend.xml) {
   Remove-Item $Env:SystemRoot\system32\Sysprep\unattend.xml -Force
 }
@@ -14,7 +15,9 @@ while($true) {
   if ($imageState -eq 'IMAGE_STATE_GENERALIZE_RESEAL_TO_OOBE') { break }
   Start-Sleep -s 5
 }
-Write-Host 'Waiting for 5 mins'
-Start-Sleep -s 300
+# Write-Host 'Waiting for 5 mins'
+# Start-Sleep -s 300
+Write-Host 'Waiting for 1 min'
+Start-Sleep -s 60
 Write-Host 'Waiting Completed'
 Write-Host '>>> Sysprep complete ...'
