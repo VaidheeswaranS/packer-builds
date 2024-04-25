a08468f8c04c4b78de37ec177ab230b32e6ebdcc
Vaidheeswaran Srinivasan <vsrinivasan@presidio.com>
2024-04-25 18:15:13 +0530

    commented some stages due to testing

--- build/images/baseline-generalized/scripts/consolidated-scripts.ps1
+++ build/images/baseline-generalized/scripts/consolidated-scripts.ps1
@@ -1,12 +1,14 @@
-# TODO: Move to Application Team Sub Blob
-$blobUrl =  "https://buildserverartifacts.blob.core.windows.net/artifacts"
+# # TODO: Move to Application Team Sub Blob
+# $blobUrl =  "https://buildserverartifacts.blob.core.windows.net/artifacts"
 
-#Install AzCopy
-echo "Installing Azcopy"
-choco install azcopy10 -y
-#Install Azure-CLI
-echo "Installing azure-cli"
-choco install azure-cli -y
+# #Install AzCopy
+# echo "Installing Azcopy"
+# choco install azcopy10 -y
+# #Install Azure-CLI
+# echo "Installing azure-cli"
+# choco install azure-cli -y
 
-#Azcopy copy command to copy the installers
-azcopy copy $blobUrl "C:\Windows\Temp" --recursive=true
+# #Azcopy copy command to copy the installers
+# azcopy copy $blobUrl "C:\Windows\Temp" --recursive=true
+
+Write-Host "This is generalized Image"
